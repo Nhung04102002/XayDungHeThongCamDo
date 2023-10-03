1ab437c1b3f68e5dbdd2629099d284801fba9afa
Nhung04102002 <doanthaonhung@gmail.com>
2023-10-03 17:05:40 +0700

    add source code

---
+++ XDHTCD/src/main/java/com/xdhtcd/XdhtcdApplication.java
@@ -0,0 +1,13 @@
+package com.xdhtcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdhtcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdhtcdApplication.class, args);
+    }
+
+}
---
+++ XDHTCD/src/test/java/com/xdhtcd/XdhtcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdhtcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdhtcdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
